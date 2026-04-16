302c1dc32910fa4f6dd446c2a2276da1175f7a6f
Crickeitz <hiredenakite@gmail.com>
2026-04-16 14:04:27 -0400

    Add files via upload

---
+++ animation/createDefaultAnimationManager.js
@@ -0,0 +1,9 @@
+import {
+    createAnimateManager
+} from './AnimationManager';
+import {
+    RequestAnimationFrameTimeoutController
+} from './timeoutController';
+export function createDefaultAnimationManager() {
+    return createAnimateManager(new RequestAnimationFrameTimeoutController());
+}
